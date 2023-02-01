6e9300f5dcb12b5c5ddced4ec00d30de9b773440
miguel.gut <miguel.gutierrez.jariod@upc.edu>
2023-02-01 13:38:56 +0100

    resume data, not finished

---
+++ src/resumeData.js
@@ -0,0 +1,124 @@
+let resumeData = {
+    "imagebaseurl":"https://MikierXXV.github.io/",
+    "name": "Miguel Gutiérrez Jariod",
+    "role": "Student of Computer Engineering",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, read books of philosophy, play the spanish guitar or simply watch series in my free time.",
+    "socialLinks":[
+        {
+            "name":"linkedin",
+            "url":"https://www.linkedin.com/in/miguel-gutjar/",
+            "className":"fa fa-linkedin"
+        },
+        {
+            "name":"github",
+            "url":"https://www.github.com/MikierXXV",
+            "className":"fa fa-github"
+        }
+    ],
+    "aboutme":"I am currently a student of Computer Engineering at the University of Barcelona, in my last year of the degree. I am passionate about learning new technologies and I am always looking for new challenges to face.",
+    "address":"Spain, Barcelona",
+    "email":"miguel.gutjar@gmail.com",
+    "phone":"(+34) 649 168 840",
+    "website":"https://www.github.com/MikierXXV",
+    "education":[
+        {
+            "UniversityName":"Polytechnic University of Catalonia",
+            "specialization":"Software Engineering",
+            "datefrom":"2018 - In progress",
+        },
+        {
+            "UniversityName":"Garbi Pere Vergés School",
+            "specialization":"Baccalaureate in Science and Technology",
+            "datefrom":"2016 - 2018",
+        },
+        {
+            "UniversityName":"Garbi Pere Vergés School",
+            "specialization":"High School",
+            "datefrom":"2012 - 2016",
+        }
+    ],
+    work : [
+        {
+            "CompanyName":"Polytechnic University of Catalonia ETSEIB",
+            "description":"Computer installation and maintenance",
+            "datefrom":"2022 - In progress",
+        },
+        {
+            "CompanyName":"COYMA",
+            "description":"Practice in a company",
+            "datefrom":"2017 - 2018",
+        },
+    ],
+    skills: [
+        {
+            "skillname":"Constant"
+        },
+        {
+            "skillname":"Constant"
+        },
+        {
+            "skillname":"Constant"
+        },
+        {
+            "skillname":"Constant"
+        },
+        {
+            "skillname":"Constant"
+        },
+        {
+            "skillname":"Constant"
+        },
+    ],
+    tecnologies: [
+        {
+            "tecnologyname":"HTML5"
+        },
+        {
+            "tecnologyname":"CSS"
+        },
+        {
+            "tecnologyname":"JavaScript"
+        },
+        {
+            "tecnologyname":"Python"
+        },
+        {
+            "tecnologyname":"Flutter"
+        },
+        {
+            "tecnologyname":"C++"
+        },
+        {
+            "tecnologyname":"C"
+        },
+        {
+            "tecnologyname":"Java"
+        },
+    ],
+    projects: [
+        {
+            "name":"Project 1",
+            "description":"mobileapp",
+            "imgurl":"images/portfolio/phone.jpg"
+        },
+        {
+            "name":"Project 2",
+            "description":"porfolio",
+            "imgurl":"images/portfolio/phone.jpg"
+        },
+        {
+            "name":"Project 3",
+            "description":"Layup web page",
+            "imgurl":"images/portfolio/phone.jpg"
+        },
+        {
+            "name":"Project 4",
+            "description":"Recomender system",
+            "imgurl":"images/portfolio/phone.jpg"
+        }
+    ],
+}
+
+export default resumeData
