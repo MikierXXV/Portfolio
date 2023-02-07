be3f6176915afd3ad9c82dd5d1bc03b693ef25fb
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-02-07 21:37:10 +0100

    changes in aspects about

--- src/components/About.js
+++ src/components/About.js
@@ -2,27 +2,31 @@ import React, {Component} from "react";
 import resumeData from "../resumeData";
 
 export default class About extends Component {
+
     render() {
         //let resumeData = this.props.resumeData;
         return(
             <section id="about">
                 <div className="row">
-                    <div className="three columns">
-                        <img src="images/CVfoto-rounded.png" alt="porfolio pic" style={{height: 240, width: 240}} />
-                    </div>
+
                     <div className="nine columns main-col">
-                        <h2>About Me</h2>
+                        <h1 style={{color: "white"}}>About Me</h1>
+                        <hr/>
                         <p>{resumeData.aboutme}</p>
-                        <div className="row">
-                            <div className="columns contact-details">
-                                <h2>Contact Details</h2>
-                                <p className="address">
-                                    <span>{resumeData.name}</span><br />
-                                    <span>{resumeData.address}</span><br />
-                                    <span>{resumeData.website}</span>
-                                </p>
-                            </div>
-                        </div>
+                        <button className="download" onClick={console.log("hello")} style={{width: 225}}> Send me a Message </button>
+                        <div style={{height: 40 }}></div>
+                        <h1 style={{color: "white"}}>Hobbies</h1>
+                        <hr/>
+                        <p>{resumeData.roleDescription}</p>
+                    </div>
+                    <div className="three columns">
+                        <img src="images/CVfoto-rounded.png" alt="porfolio pic" style={{height: 300, width: 280}} />
+                        <h2>Contact Details</h2>
+                        <p className="address">
+                            <span><span style={{color:"#a8379f"}}>Name:  </span>{resumeData.name}</span><br />
+                            <span><span style={{color:"#a8379f"}}>Address:  </span>{resumeData.address}</span><br />
+                            <span><span style={{color:"#a8379f"}}>Email:  </span>{resumeData.email}</span>
+                        </p>
                     </div>
                 </div>
             </section>
