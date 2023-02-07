344848cb6337f8dc4db03f8b441965922a67bd1d
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-02-07 21:36:05 +0100

    Home out

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
