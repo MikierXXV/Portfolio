e3076c929141bb65f1887a181082ea82424aacd4
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-06-12 22:01:33 +0200

    Updates

---
+++ precache-manifest.95d7396c1beeb734480510f528dead4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6b8954cac29329f424",
+    "url": "/Portfolio/static/css/main.080128fd.chunk.css"
+  },
+  {
+    "revision": "8f6b8954cac29329f424",
+    "url": "/Portfolio/static/js/main.062c7de7.chunk.js"
+  },
+  {
+    "revision": "84890ed0cd523156f394",
+    "url": "/Portfolio/static/js/runtime~main.95a7a821.js"
+  },
+  {
+    "revision": "3587e2ef8c2824dc9fcd",
+    "url": "/Portfolio/static/css/2.ca80fcef.chunk.css"
+  },
+  {
+    "revision": "3587e2ef8c2824dc9fcd",
+    "url": "/Portfolio/static/js/2.39a39f3d.chunk.js"
+  },
+  {
+    "revision": "bbb85af136b7dcd525ee",
+    "url": "/Portfolio/static/js/3.518f373d.chunk.js"
+  },
+  {
+    "revision": "fd0d30ee86a1f3f7f25e53d715b92637",
+    "url": "/Portfolio/index.html"
+  }
+];
