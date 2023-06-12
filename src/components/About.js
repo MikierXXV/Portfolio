91d6f8d59dbf5a5640b3881635efcb30e83e5635
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-06-12 20:41:06 +0200

    Dev: Little changes color font + web navbar

--- src/components/About.js
+++ src/components/About.js
@@ -18,25 +18,25 @@ export default class About extends Component {
                     <div className="nine columns main-col">
                         <h1 style={{color: "white"}}>About Me</h1>
                         <hr/>
-                        <p>{resumeData.aboutme}</p>
+                        <p style={{color: "whitesmoke"}}>{resumeData.aboutme}</p>
                         <button className="download" onClick={() => { redirectEmail()}} style={{width: 225}}> Send me a Message </button>
                         <div className="space"></div>
                         <h1 style={{color: "white"}}>Hobbies</h1>
                         <hr/>
-                        <p>{resumeData.roleDescription}</p>
+                        <p style={{color: "whitesmoke"}}>{resumeData.roleDescription}</p>
                         <div className="penta">
-                            <a href={""}><i className={"fa-3x"}><FaBasketballBall/></i></a>
-                            <a href={""}><i className={"fa-3x"}><FaFilm/></i></a>
-                            <a href={""}><i className={"fa-3x"}><FaGuitar/></i></a>
-                            <a href={""}><i className={"fa-3x"}><GiBookshelf/></i></a>
-                            <a href={""}><i className={"fa-3x"}><FaLaptopCode/></i></a>
+                            <a href={"#about"}><i className={"fa-3x"}><FaBasketballBall/></i></a>
+                            <a href={"#about"}><i className={"fa-3x"}><FaFilm/></i></a>
+                            <a href={"#about"}><i className={"fa-3x"}><FaGuitar/></i></a>
+                            <a href={"#about"}><i className={"fa-3x"}><GiBookshelf/></i></a>
+                            <a href={"#about"}><i className={"fa-3x"}><FaLaptopCode/></i></a>
                         </div>
                     </div>
                     <div className="three columns">
                         <img src="images/CVfoto-rounded.png" alt="porfolio pic" style={{height: 300, width: 280}} />
                         <h2>Contact Details</h2>
                         <p className="address">
-                            <span><span style={{color:"#a8379f"}}>Name:  </span>{resumeData.name}</span><br />
+                            <span><span style={{color:"#a8379f"}}>Name:  </span >{resumeData.name}</span><br />
                             <span><span style={{color:"#a8379f"}}>Address:  </span>{resumeData.address}</span><br />
                             <span><span style={{color:"#a8379f"}}>Email:  </span>{resumeData.email}</span>
                         </p>
